e57efbfb37ff1f45ed81e06a43b5f3060279e152
Gideon <gideonyeboah0@gmail.com>
2024-06-11 07:02:11 +0000

    first php lesson

---
+++ Code Challenge (10/06/2024)/index.php
@@ -0,0 +1,18 @@
+<?php 
+$firstName = "Gideo"; // Student first Name//
+$lastName = "Yeboah";  //  Student Last Name//
+$middleNamw="";      //  Student middle Name//
+$Gender="Male";      // Student gender//
+$DOB= "3rd May 2007";    //Student date of birth//
+$stuClass="Class 5";   // Student class//
+$admissionDate="10/04/2025";    //  Student date of admission//
+
+// displaying  data in the form of myself with records from the variable.//
+echo "Hello, my name is $firstName $lastName $middleNamw .; 
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate. <br>
+THANK YOU!";
+
+?> 
