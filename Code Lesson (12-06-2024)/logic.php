830af964e890041966800f5b90ac0ca1d5727713
gafis <gafis@gafis.com>
2024-06-13 01:02:57 +0100

    Added Lesson 2 and its code challenge and also changed folde date format

---
+++ Code Lesson (12-06-2024)/logic.php
@@ -0,0 +1,24 @@
+<?php
+    $name = 'NABY';
+    $age = 18;
+    
+    // $response = '';
+
+    if($age > 18) {
+        $response = "Hello $name, you qualify to vote. You are $age years old";
+    }else if($age < 17) {
+
+    }else if($age < 10) {
+
+    }else if($age < 5) {
+
+    } else {
+
+    }
+
+    // if($age > 18) {
+    //     $response = "Hello $name, you qualify to vote. You are $age years old";
+    // } else {
+    //     $response = "Sorry $name, you dont qualify to vote. You are $age years old";
+    // }
+?>
