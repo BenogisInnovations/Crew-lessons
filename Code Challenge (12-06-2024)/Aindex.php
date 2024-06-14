c446e7254fd4cad6981966363df0d2c3b2e1c5be
NanaYawAustin <yaw292@gmail.com>
2024-06-14 12:51:26 +0000

    Made modifications with my table display

--- Code Challenge (12-06-2024)/Aindex.php
+++ Code Challenge (12-06-2024)/Aindex.php
@@ -7,10 +7,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin: 20px 0;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 <body>
     <h1> Report sheet </h1>
-    <table border="1">
+    <table>
     <tr>
         <th>Name</th>
         <th>Class</th>
