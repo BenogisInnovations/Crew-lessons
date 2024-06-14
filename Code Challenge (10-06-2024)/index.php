3e55f2eb8272a954b80e4da63571a61d589c0028
Gideon <gideonyeboah0@gmail.com>
2024-06-14 17:00:45 +0000

    answee coding challenge 10-06-2024

--- Code Challenge (10-06-2024)/index.php
+++ Code Challenge (10-06-2024)/index.php
@@ -1,18 +1,246 @@
 <?php 
-$firstName = "Gideo"; // Student first Name//
-$lastName = "Yeboah";  //  Student Last Name//
-$middleNamw="";      //  Student middle Name//
-$Gender="Male";      // Student gender//
-$DOB= "3rd May 2007";    //Student date of birth//
-$stuClass="Class 5";   // Student class//
-$admissionDate="10/04/2025";    //  Student date of admission//
+$firstName = "Gideon"; // Student first Name
+$lastName = "Yeboah";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Male";      // Student gender
+$DOB= "3rd May 2007";    //Student date of birth
+$stuClass="Class 5";   // Student class
+$admissionDate="10/04/2025";    //  Student date of admission
 
-// displaying  data in the form of myself with records from the variable.//
-echo "Hello, my name is $firstName $lastName $middleNamw .; 
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " Hello, my name is $firstName $lastName $middleName .
 I am a $Gender. <br>
 I was born on the $DOB. 
 I am in class $stuClass. <br> 
-I was admitted on the $admissionDate. <br>
+I was admitted on the $admissionDate. 
+THANK YOU! <br>";
+
+/*********************************** Second  student ******************/
+$firstName = "Mensah"; // Student first Name
+$lastName = "Prince";  //  Student Last Name
+$middleName="Kofi";      //  Student middle Name
+$Gender="Male";      // Student gender
+$DOB= "15th May 2007";    //Student date of birth
+$stuClass="Class 2";   // Student class
+$admissionDate="10/04/2025";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable. *******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br> ";
+
+/***********************************  Third  student ******************/
+$firstName = "Samuel"; // Student first Name
+$lastName = "Forson";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Male";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 6";   // Student class
+$admissionDate="10/04/2021";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br>";
+
+/*********************************** Fourth student ******************/
+$firstName = "Iddriss "; // Student first Name
+$lastName = "Salifu";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Male";      // Student gender
+$DOB= "3rd June  2007";    //Student date of birth
+$stuClass="Class 5";   // Student class
+$admissionDate="10/04/2025";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName . 
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br>";
+/*********************************** fifth student ******************/
+$firstName = "Amo"; // Student first Name
+$lastName = "Serwaa";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2012";    //Student date of birth
+$stuClass="Class 9";   // Student class
+$admissionDate="10/04/2024";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br>";
+/*********************************** Sixth student ******************/
+$firstName = "Janet"; // Student first Name
+$lastName = "Oppong";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="female";      // Student gender
+$DOB= "3rd August 2007";    //Student date of birth
+$stuClass="Class 1";   // Student class
+$admissionDate="10/04/2020";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br>";
+/*********************************** Seventh student ******************/
+$firstName = "Emmanuela "; // Student first Name
+$lastName = "Adu";  //  Student Last Name
+$middleName="Poku";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 3";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate.
+THANK YOU!<br>";
+
+/*********************************** Eighth student ******************/
+$firstName = "Emmanuel"; // Student first Name
+$lastName = "Adu";  //  Student Last Name
+$middleName="Poku";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 3";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br>";
+/*********************************** Nineth student ******************/
+$firstName = "Sampson"; // Student first Name
+$lastName = "Arhin";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="male";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 3";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU!<br>";
+/*********************************** Tenth student ******************/
+$firstName = "Emmanuel "; // Student first Name
+$lastName = "Frimpong";  //  Student Last Name
+$middleName="Oti";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 20112";    //Student date of birth
+$stuClass="Class 6";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/******* displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU! <br>";
+/*********************************** eleventh student ******************/
+$firstName = "Afis "; // Student first Nam
+$lastName = "Osei";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 5";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/**************** displaying  data in the form of myself with records from the variable.*******/
+echo " <br> Hello, my name is $firstName $lastName $middleName . 
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU!<br>";
+/*********************************** Twelveth student ******************/
+$firstName = "Elvis"; // Student first Name
+$lastName = "Asare";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 3";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+// displaying  data in the form of myself with records from the variable.
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU!<br>";
+/*********************************** Thirteenth student ******************/
+$firstName = "Pamella"; // Student first Name
+$lastName = "Adu";  //  Student Last Name
+$middleName="Poku";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 3";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/***displaying  data in the form of myself with records from the variable.*****/
+echo " <br> Hello, my name is $firstName $lastName $middleName . 
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU!<br>";
+/*********************************** Forteenth student ******************/
+$firstName = "Doreen "; // Student first Name
+$lastName = "Adu";  //  Student Last Name
+$middleName="Nsiah";      //  Student middle Name
+$Gender="Female";      // Student gender
+$DOB= "3rd May 2017";    //Student date of birth
+$stuClass="Class 3";   // Student class
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/******displaying  data in the form of myself with records from the variable.*****/
+echo " <br> Hello, my name is $firstName $lastName $middleName .
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
+THANK YOU!<br>";
+/*********************************** Fifteenth student ******************/
+$firstName = "Doris "; // Student first Name
+$lastName = "Konadu";  //  Student Last Name
+$middleName="";      //  Student middle Name
+$Gender="Female";      // Student gender//
+$DOB= "3rd April 2017";    //Student date of birth
+$stuClass="Class 5";   // Student class//
+$admissionDate="10/04/2015";    //  Student date of admission
+
+/*****displaying  data in the form of myself with records from the variable.**/
+echo " <br> Hello, my name is $firstName $lastName $middleName . 
+I am a $Gender. <br>
+I was born on the $DOB. 
+I am in class $stuClass. <br> 
+I was admitted on the $admissionDate
 THANK YOU!";
 
 ?> 
