5d2b1550531c19f771f739581f7f4146ce482454
Danny-Boy <agyekumdaniel29@gmail.com>
2024-07-03 09:39:51 +0000

    Calculator

---
+++ Code Challenge 05 (30-06-2024)/Answer.php
@@ -0,0 +1,33 @@
+<?php
+// Declaring my function
+function calculator($num1, $num2,$operator){ 
+    //I'm issueing command on how th operators shold work
+    switch ($operator) {
+    case '+':
+        return $num1 + $num2;
+    case '-':
+        return $num1 - $num2;
+    case '*':
+        return $num1 * $num2;
+    case '/':
+        // the != is called is not equal to
+        if ($num2 != 0) {
+            return $num1 / $num2;
+        } else {
+            return "Please dvison by 0 is udefined";
+        }
+    default:
+        return "I accept only Divison, Multiplication, Subtraction and Addition";
+}
+}
+    
+$num1 = 10;
+$num2 = 0;
+$operator = '_';
+$result = calculator($num1, $num2,$operator);
+echo"Result: " . $result;
+
+
+    ?>
+
+
