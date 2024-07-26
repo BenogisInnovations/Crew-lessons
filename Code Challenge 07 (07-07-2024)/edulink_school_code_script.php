ba19a60c80cdb805eba4deba2d9a5460852405a9
Danny-Boy <agyekumdaniel29@gmail.com>
2024-07-26 18:19:45 +0000

    update

--- Code Challenge 07 (07-07-2024)/edulink_school_code_script.php
+++ Code Challenge 07 (07-07-2024)/edulink_school_code_script.php
@@ -4,13 +4,13 @@
 class EduLinkSchoolCode {
 
     // This function returns the edulink school code for client schools
-    function getSchoolCode($region, $schoolType, $schoolPrefix){ 
+    public function getSchoolCode($region, $schoolType, $schoolPrefix){ 
         // STEP 1: Get the region the school is in
         // STEP 2: Get the school type | Private or Public
         // STEP 3: Get the school character prefix
         // STEP 4: check if school code exists and append the @edulink domain
         $schoolCode = $region . $schoolType . $schoolPrefix;
-        $edulinkDomain = ''; // edulink domain
+        $edulinkDomain = '@edulink'; // edulink domain
         $SchoolfullCode = $schoolCode . $edulinkDomain;// concatenating $schoolcode and edulink domain to get my full code for a school
         return $SchoolfullCode;
 
@@ -18,7 +18,7 @@ class EduLinkSchoolCode {
     
     // This function will return the two character region code. eg: Ashanti Region = AK
     // AK, GA, BA, WR, ER, UW, NR, AR, SR, OR, CR, WN, UE, NE, VR, BE
-    function getSchoolRegion(string $school_region){
+    public function getSchoolRegion(string $school_region){
         // we have passed the school region to get each word capitalized by first letter
         $school_region = ucwords($school_region); 
         // I'm using switch cases to get only the abbrev of a region
@@ -78,7 +78,7 @@ class EduLinkSchoolCode {
     }
     
     // This function will return the school type being 1 for Private and 2 for Public
-    function getSchoolType(string $type){
+    Public function getSchoolType(string $type){
         // we have passed the school region to get each word capitalized by first letter
         $type = strtolower($type);
 
@@ -92,7 +92,7 @@ class EduLinkSchoolCode {
     }
     
     // This function will return the 3 to 5 character school prefix eg: TPMAJ
-    function getSchoolPrefix($schoolName){
+    public function getSchoolPrefix($schoolName){
         // I'm using switch cases to get only the abbrev of a schoolname
         switch ($schoolName) {
             case 'Grace Action Faith':
@@ -104,11 +104,25 @@ class EduLinkSchoolCode {
             case 'New World Living School':
                 return 'NWLS';
                 break;
+            default:
+                return 'invalid school prefix';
     }
-    
+    }
     // This function will returnthe school code verified with the edulink domain
-    function verifySchoolCodeAndAppendDomain($schoolCode){
+     Public function verifySchoolCodeAndAppendDomain($schoolCode){
     
     }
 }
-}
+
+
+// the output
+$edulink = new EduLinkSchoolCode();
+$region = "Ashanti Region";
+$schoolType = "Private";
+$schoolPrefix = "Grace Action Faith";
+
+// Get the full school code
+$SchoolfullCode = $edulink->getSchoolCode($region, $schoolType, $schoolPrefix);
+echo $SchoolfullCode; // this is how it's suppose to appear  AK1GAF@edulink
+
+?>
